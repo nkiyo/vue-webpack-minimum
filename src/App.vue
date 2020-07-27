a42ac1c9fefec463a7ed4dfdb02d334cd107d4ef
nkiyo <nkiyo@hoge.com>
2020-07-28 08:28:32 +0900

    up

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
     <p>hello {{ who }}</p>
+    <p>hello {{ reversedWho }}</p>
+    <button @click="onclick">hoge</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "app",
-  data() {
+  data: function() {
     return {
-      who: "monk studio"
+      who: "monk studio",
+      /** @type {{id: number, text: string}[]} */
+      items: []
     };
+  },
+  computed: {
+    reversedWho: function() {
+      return this.who
+        .split("")
+        .reverse()
+        .join("");
+    }
+  },
+  methods: {
+    onclick: function() {
+      this.items.push(123)
+      console.log(`${this.items.length}`)
+    }
   }
 };
 </script>
